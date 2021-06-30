d2ba0b26fef59beb07ed7e23ef9c394ec81ce415
徐彬 <729044639@qq.com>
2021-06-30 16:47:17 +0800

    20210629 config-Server集群暂时跳过

--- config-client/src/main/java/com/example/ConfigClientApplication.java
+++ config-client/src/main/java/com/example/ConfigClientApplication.java
@@ -2,14 +2,12 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableEurekaClient
-@EnableConfigServer
 @SpringBootApplication
-public class ConfigServerApplication {
+public class ConfigClientApplication {
     public static void main(String[] args) {
-        SpringApplication.run(ConfigServerApplication.class, args);
+        SpringApplication.run(ConfigClientApplication.class, args);
     }
 }
